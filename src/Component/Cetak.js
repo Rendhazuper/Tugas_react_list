3d5e6e70da927fec2beadd792780ff0542e055c4
Rendha <rendhazuper@gmail.com>
2022-02-23 14:33:08 +0700

    Learn React

---
+++ src/Component/Cetak.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class Cetak extends React.Component{
+    render(){
+        return(
+            <div className="main"><h2>HALAMAN CETAK</h2></div>
+        )
+    }
+}
+
+export default Cetak;
---
+++ src/Component/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Home extends React.Component {
+    render(){
+        return(
+            <div className="home-bg">
+                <div className="container">
+                    <h1 className="sambutan">SELAMAT DATANG DIDUNIA BARU</h1>
+                    <h5>REACT JS</h5>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
